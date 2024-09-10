ae65fddeb81631dd78d5d24cf0b6cc3255463a74
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-09-09 17:23:28 -0700

    Create ProjectsRepository.cs

---
+++ Services/ProjectsRepository.cs
@@ -0,0 +1,29 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Services
+{
+    public class ProjectsRepository
+    {
+        public List<ProjectDTO> getProjects()
+        {
+            return new List<ProjectDTO>() {
+                new ProjectDTO
+            {
+                Title = "Amazon",
+                Description = "E-Commerse system developed on ASP.NET Core",
+                Link = "https://amazon.com",
+                ImageUrl = "/images/amazon.png"
+            },
+               new ProjectDTO
+            {
+                Title = "New York Time",
+                Description = "Newletter page on react",
+                Link = "https://nytimes.com",
+                ImageUrl = "/images/nyt.png"
+            }
+            };
+
+        }
+
+    }
+}
