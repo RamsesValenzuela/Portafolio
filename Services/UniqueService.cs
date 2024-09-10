d6a1cb29794e7034b8e37fdb39f217d83536f766
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-09-10 16:08:38 -0700

    Create UniqueService.cs

---
+++ Services/UniqueService.cs
@@ -0,0 +1,39 @@
+﻿namespace Portafolio.Services
+{
+    public class UniqueService
+    {
+        public UniqueService()
+        {
+            getGuid = Guid.NewGuid();
+        }
+
+        public Guid getGuid { get; private set; }
+
+    }
+
+    public class DelimitedService
+    {
+        public DelimitedService()
+        {
+            getGuid = Guid.NewGuid();
+        }
+
+        public Guid getGuid { get; private set; }
+
+    }
+
+    public class TransientService
+    {
+        public TransientService()
+        {
+            getGuid = Guid.NewGuid();
+        }
+
+        public Guid getGuid { get; private set; }
+
+    }
+
+
+
+
+}
