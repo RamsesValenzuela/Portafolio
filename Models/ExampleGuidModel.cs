92af1e80cb0b2662e7f2a3a99fcf2d5ac0ef7a7f
RamsesValenzuela <92767815+RamsesValenzuela@users.noreply.github.com>
2024-09-10 16:08:30 -0700

    Create ExampleGuidModel.cs

---
+++ Models/ExampleGuidModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Portafolio.Models
+{
+    public class ExampleGuidModel
+    {
+        public Guid Transitorio { get; set; }
+        public Guid Unico { get; set; }
+        public Guid Delimitado { get; set; }
+
+    }
+}
